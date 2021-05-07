f824c8236deb55c8861ae4632b9da02583d21122
Szabó Tamás <szabotomika97@gmail.com>
2021-05-07 14:40:23 +0200

    pw deleted from sourcecode

--- server/index.js
+++ server/index.js
@@ -1,57 +1,3 @@
-/*const express = require('express');
-const cors = require('cors')
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
-const expressSession = require('express-session');
-
-const app = express();
-
-const port = process.env.PORT || 3000;
-const dbUrl = 'mongodb+srv://admin:kotprog12345678@cluster0.sb5ut.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
-
-mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
-
-mongoose.connection.on('connected', () => {
-    console.log('db csatlakoztatva');
-})
-
-mongoose.connection.on('error', (err) => {
-    console.log('Hiba tortént', err);
-})
-
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({}));
-
-const whiteList = ['http://localhost:4200'];
-
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
-    next();
-});
-
-
-
-app.use('/',require('./routes'));
-
-app.listen(port, () => {
-    console.log('The server is running on port ' + port + '!');
-});
-
-*/
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -60,7 +6,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-const dbUrl = 'mongodb+srv://admin:kotprog12345678@cluster0.sb5ut.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const dbUrl = 'mongodb+srv://admin:<pw>@cluster0.sb5ut.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
 
 mongoose.connection.on('connected', () => {
