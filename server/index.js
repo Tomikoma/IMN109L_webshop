8a29c7ea8fa4b700f020cfbe6d9ef052fd434f7d
Szabó Tamás <szabotomika97@gmail.com>
2021-05-07 16:43:13 +0200

    signup fixed, restapi for login,logout implemented

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const expressSession = require('express-session');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 const dbUrl = 'mongodb+srv://admin:<pw>@cluster0.sb5ut.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
 mongoose.connection.on('connected', () => {
     console.log('db csatlakoztatva');
@@ -18,24 +23,52 @@ mongoose.connection.on('error', (err) => {
 })
 
 
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
-    next();
+  res.set('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.set('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+      // Send response to OPTIONS requests
+      res.set('Access-Control-Allow-Methods', 'GET');
+      res.set('Access-Control-Allow-Headers', 'Content-Type');
+      res.set('Access-Control-Max-Age', '3600');
+  }
+  next();
+})
+
+const User = require('./models/user');
+
+passport.use('local', new localStrategy({usernameField: 'email', passwordField: 'password'},function (email, password, done) {
+  User.findOne({ email: email }, function (err, user) {
+      if (err) return done('Hiba lekeres soran', null);
+      if (!user) return done('Nincs ilyen felhasználónév', null);
+      user.comparePasswords(password, function (error, isMatch) {
+          if (error) return done(error, false);
+          if (!isMatch) return done('Hibas jelszo', false);
+          return done(null, user);
+      })
+  })
+}));
+
+passport.serializeUser(function (user, done) {
+  if (!user) return done('nincs megadva beléptethető felhasználó', null);
+  return done(null, user);
 });
 
+passport.deserializeUser(function (user, done) {
+  if (!user) return done("nincs user akit kiléptethetnénk", null);
+  return done(null, user);
+});
+
+app.use(expressSession({ secret: 'prfkotprogihopethisworksnow', resave: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 const routes = require('./routes');
+
 
 app.use('/api', routes);
 //app.use('/api/product', productRoutes);
--- server/routes.js
+++ server/routes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const User = require('./models/user');
 
-router.post('user/signup',(req,res,next) => {
+router.post('/user/signup',(req,res,next) => {
     email = req.body.email;
     password = req.body.password;
     fullname = req.body.fullname;
     if (email && password && fullname && fullname.family && fullname.first){
-        const user = new User({email:email,password:password,fullname:fullname});
+        const user = new User({email:email,password:password,name:fullname});
         user.save((error) => {
             if (error) return res.status(500).json({
                 message: 'Probláma lépett fel regisztráció közben!',
@@ -21,8 +22,32 @@ router.post('user/signup',(req,res,next) => {
     }
 });
 
-router.post('login',(req,res,next) => {
+router.post('/user/login',(req,res,next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    if(email, password) {
+        passport.authenticate('local',function(error, user) {
+            if(error) {
+                console.log(error);
+                return res.status(500).json({message: 'Probléma történt bejelentkezés közben!', error: error});
+            }
+            req.login(user, function(err) {
+                if(error) return res.status(500).json({message: 'Probléma történt bejelentkezés közben!', error: err});
+                return res.status(200).json({message:'Bejelentkezes sikeres'});
+            })
+        })(req, res);
+    } else {
+        return res.status(400).send('Hibas keres, username es password kell');
+    }
+});
 
+router.post('/user/logout', (req,res,next) => {
+    if(req.isAuthenticated()) {
+        req.logout();
+        return res.status(200).json({message: 'Kijelentkezes sikeres'});
+    } else {
+        return res.status(403).json({message: 'Nem is volt bejelentkezve'});
+    }
 });
 
 module.exports = router;
