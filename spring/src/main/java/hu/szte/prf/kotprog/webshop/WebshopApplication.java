1643bc539ed71c8080d6cd5800337760e277b42b
Szabó Tamás <szabotomika97@gmail.com>
2021-05-10 12:27:53 +0200

    spring init

---
+++ spring/src/main/java/hu/szte/prf/kotprog/webshop/WebshopApplication.java
@@ -0,0 +1,13 @@
+package hu.szte.prf.kotprog.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopApplication.class, args);
+	}
+
+}
